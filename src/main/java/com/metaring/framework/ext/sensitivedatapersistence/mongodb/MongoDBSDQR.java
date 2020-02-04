dd5bdd77fcb3bcbd8de355b4400937dfcebfb537
Marco Vasapollo <ceo@metaring.com>
2020-02-04 10:00:11 +0100

    LastIndexOf bugfix

--- src/main/java/com/metaring/framework/ext/sensitivedatapersistence/mongodb/MongoDBSDQR.java
+++ src/main/java/com/metaring/framework/ext/sensitivedatapersistence/mongodb/MongoDBSDQR.java
@@ -1,12 +1,12 @@
 /**
  *    Copyright 2019 MetaRing s.r.l.
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ public final class MongoDBSDQR implements SensitiveDataQueryResolver {
             query = query.substring(0, query.length() - 1);
         }
         String toFirstBraceSplit = query.substring(0, query.indexOf("("));
-        String jsonQuery = "[" + query.substring(query.indexOf("(") + 1, query.indexOf(")")) + "]";
+        String jsonQuery = "[" + query.substring(query.indexOf("(") + 1, query.lastIndexOf(")")) + "]";
         DataRepresentation dataFromQuery = Tools.FACTORY_DATA_REPRESENTATION.fromJson(jsonQuery);
         DataRepresentation blurredData = await(tarnishWithParams(SensitiveDataInfo.create(dataFromQuery, params)));
         String blurredDataJson = blurredData.toJson();
